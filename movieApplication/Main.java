477963b51d50e256b29a7cecfb8d734271e73690
Your Name <you@example.com>
2023-08-15 01:38:17 +0530

    movieApplication

---
+++ movieApplication/Main.java
@@ -0,0 +1,9 @@
+package com.techlab.movieApplication;
+
+public class Main {
+	public static void main(String[] args) {
+	    MovieController controller = new MovieController();
+	    controller.start();
+	  }
+
+}
